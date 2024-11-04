c2e45e501d3a6414339687e8db950ef6370c9fb6
John Rhay Domingo <jhnrhyyyyyyyyyy@gmail.com>
2024-11-04 17:50:47 +0800

    User Profile

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "John Rhay Domingo";
+let age = "21";
+let favoriteNumber = "17";
+let favoriteColor = "Black";
+
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
